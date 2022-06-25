00b79ba295490653e5ce8718f502b678f0a406e4
tidalcode <mcy03131993@gmail.com>
2022-06-25 01:35:22 -0500

    got computer function with random rps choices.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function computer() {
-        var randomlist = [
-        "Rock",
-        "Paper",
-        "Scissors"
-    ];
+let rpsArray = [
+    'Rock',
+    'Paper',
+    'Scissors'
+]
+ 
+//var randomItem = rpsArray[Math.floor(Math.random() * rpsArray.length)];
 
-    let computerlist = randomlist[Math.floor(Math.random()*randomlist.length)];
+function computer(){
+    let choice = rpsArray [Math.floor(Math.random() * rpsArray.length)];
+    let computer = choice;
 }
 
 computer()
+
+
